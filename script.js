0fce2972db5483e72608bbb7945d8a0803a3c759
mahan-redboy177 <mahanperspolis1386@gmail.com>
2025-09-15 08:24:12 +0330

    //primitve(value)

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
+//primitve(value) types
+//string
+//number
+//bool
+//undifined
+//null
 
-const name = "mahan" ;
-name = 'reza';
+//string
+let name = "mahan" ;
+//number
+let age = 32 ;
+//bool
+let admin = true ;
+//undifined
+let lname;
+//null
+let fname= null;
 
 console.log(name);
