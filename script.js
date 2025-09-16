e6c7e2d022a44ab9c579b0658a5de21c565ea1e9
mahan-redboy177 <mahanperspolis1386@gmail.com>
2025-09-16 10:21:02 +0330

    Logical operator on non-Boolean Russians

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
-// lagical AND
-//&&
-// console.log(true && false && true); 
-let ProductCount = 3;
-let ProductPrice = 1000;
+console.log(false || true);
+console.log(false || "mahan");
+console.log( false || 0 || false );
+console.log( false || 1 || false );
+console.log(false || "mahan" || false );
+console.log(false && 1);
+console.log("" && 1);
+console.log(0 && 1);
+console.log( true && 0 && "mahan");
 
-let credit = 1200;
-let buy = ProductCount > 0 && credit>ProductPrice ;
-console.log(buy);
-// logical OR
-// ||
-let ProductCount1 = -1;
-let ProductPrice1 = 1000;
-
-let credit1 = 1200;
-let buy1 = ProductCount1 > 0 || credit1>ProductPrice1 ;
-console.log(buy1);
-//not
-//!
-let ProductCount2 = 3;
-let ProductPrice2 = 1000;
-
-let credit2 = 1200;
-let buy2 = ProductCount2 > 0 || credit2>ProductPrice2 ;
-console.log(!buy2);
+// falses
+// false , undifined , null , 0 , "" , NaN
+//true , 1 ,-2   
