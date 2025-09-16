30a2c5f669ed6695646e98399788cc7fc1f7f4f7
mahan-redboy177 <mahanperspolis1386@gmail.com>
2025-09-16 11:18:36 +0330

    switch-case

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-// 6am - 12pm -> good morning
-// 12pm -6pm -> good afternoon
-//otherwise -> good evening
-let hour = 20;
 
-if(hour>=6 && hour<12){
-    console.log("good morning");
-}else if(hour>=12 && hour<18){
-    console.log("good afternoon");
+let role = '';
+
+switch(role){
+    case "guest":
+        console.log("guest user");
+        break;
+    case "admin":
+        console.log("admin user");
+        break;
+    default:
+        console.log('unknown user');
+}
+
+if(role==="guest"){
+    console.log("guest user");
+}else if(role==="admin"){
+    console.log("admin user");
 }else{
-    console.log("good evening");
+    console.log('unknown user');
 }
