e2083f71afcb4ee4a7d1aa3579a5f4f3f802e820
mahan-redboy177 <mahanperspolis1386@gmail.com>
2025-09-16 11:07:57 +0330

    IF...else

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-// 1 ,2 , 100
-// 00000001 = 1
-// 00000010 = 2
-// 00000000 = 0
- console.log( 1 | 2);
+// 6am - 12pm -> good morning
+// 12pm -6pm -> good afternoon
+//otherwise -> good evening
+let hour = 20;
 
- // bitwise AND : &
- // bitwise OR : |
+if(hour>=6 && hour<12){
+    console.log("good morning");
+}else if(hour>=12 && hour<18){
+    console.log("good afternoon");
+}else{
+    console.log("good evening");
+}
