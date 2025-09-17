441b0ef7fa5840c89961edec65000379c885af9b
mahan-redboy177 <mahanperspolis1386@gmail.com>
2025-09-17 08:44:15 +0330

    breack and continue

--- script.js
+++ script.js
@@ -1,58 +1,20 @@
-//for
-//while
-//do while
-//for .. in
-//for .. of
-
-
-//for
-for(let i=0;i<5;i++){
-    console.log("mahan")
-}
-
-for(let i=0;i<50;i++){
-    if(i % 2 == 0){
-        console.log(i);
-    }
-}
-
-for(let i=0;i<50;i++){
-    if(i % 2 == 1){
-        console.log(i);
-    }
-}
-
-for(let i=5;i>=1;i--){
-    if(i % 2 == 1){
-        console.log(i);
-    }
-}
-
-//while
 let i= 0;
-while(i<=5){
+
+while(i<=10){
+    if(i==5){
+        break;
+    }
     console.log(i);
     i++;
 }
 
-//do while
-let x = 0;
-do{
+let x= 0;
+
+while(x<=10){
+    if(x==5){
+        x++;
+        continue;
+    }
     console.log(x);
-    x++
-}while(i<5);
-
-//for .. in
-const person = {name:"mahan",age:27};
-
-for(let key in person){
-    console.log(key);
-    console.log(person[key]);
-}
-
-//for .. of
-const colors = ['red',"green","blue"]
-
-for(let color of colors){
-    console.log(color);    
+    x++;
 }
