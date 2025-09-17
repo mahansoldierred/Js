2a183e402a3421a73c57568b8e67060c0e78ce0f
mahan-redboy177 <mahanperspolis1386@gmail.com>
2025-09-17 08:38:26 +0330

    loops

--- script.js
+++ script.js
@@ -1,21 +1,58 @@
+//for
+//while
+//do while
+//for .. in
+//for .. of
 
-let role = '';
 
-switch(role){
-    case "guest":
-        console.log("guest user");
-        break;
-    case "admin":
-        console.log("admin user");
-        break;
-    default:
-        console.log('unknown user');
+//for
+for(let i=0;i<5;i++){
+    console.log("mahan")
 }
 
-if(role==="guest"){
-    console.log("guest user");
-}else if(role==="admin"){
-    console.log("admin user");
-}else{
-    console.log('unknown user');
+for(let i=0;i<50;i++){
+    if(i % 2 == 0){
+        console.log(i);
+    }
+}
+
+for(let i=0;i<50;i++){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+}
+
+for(let i=5;i>=1;i--){
+    if(i % 2 == 1){
+        console.log(i);
+    }
+}
+
+//while
+let i= 0;
+while(i<=5){
+    console.log(i);
+    i++;
+}
+
+//do while
+let x = 0;
+do{
+    console.log(x);
+    x++
+}while(i<5);
+
+//for .. in
+const person = {name:"mahan",age:27};
+
+for(let key in person){
+    console.log(key);
+    console.log(person[key]);
+}
+
+//for .. of
+const colors = ['red',"green","blue"]
+
+for(let color of colors){
+    console.log(color);    
 }
