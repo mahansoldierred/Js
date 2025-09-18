0ec78ccdc0c43f05dd455e1978fa1c72de06ee39
mahan-redboy177 <mahanperspolis1386@gmail.com>
2025-09-18 14:24:22 +0330

    Navigating object properties

--- script.js
+++ script.js
@@ -1,26 +1,29 @@
-// value types(primitive)
-//number
-//string
-//boolean
-//symblol
-//undifined
-//null
-
-//Refernce types
-//object
-//function
-//array
-
-// let x = {value : 10};
-// let y = x;
-
-// x.value = 20
-
-// console.log(x);
-
-let obj = {vlaue : 10};
-function increase(obj){
-    obj.vlaue++;
+const circle = {
+    radius : 1 ,
+    draw(){
+        console.log('draw');
+    }
 }
-increase(obj);
-console.log(obj);
+
+if('radius' in circle){
+    console.log('yes');
+    console.log('draw' in circle)
+}
+// for (let key in circle){
+//     console.log(circle[key])
+// }
+
+// for (let key of circle){
+//     console.log(key)
+// }
+
+// let circlekeys = Object.keys(circle);
+// console.log(circlekeys);
+
+// for(let key of circlekeys)
+//     console.log(key);
+
+// let circleEntries = Object.entries(circle);
+// for(let entry of circleEntries){
+//     console.log(entry);
+// }
