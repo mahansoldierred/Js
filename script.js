812e281d431bf1f798cf10db03ab4e15c5a6220e
mahan-redboy177 <mahanperspolis1386@gmail.com>
2025-09-20 11:09:00 +0330

    Object Date

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
-// const message = '\'Mahan\' . \n ' +
-//  '  coding is cool';
-
-//  // {}
-//  // true,false
-//  //' "
-//  // `
- 
-// const another = `'mahan ' .
-// coding is cool
-// `;
-
-let name = 'Roghayeh';
-let msg = `hi ${  name } . thank you`;
-console.log(msg)
+const now = new Date();
+const data1 = new Date('may 11 2026 09:00');
+const date2 = new Date(2018,4,11,9,0);
+//date
+now.toDateString();
+//date and hour
+now.toISOString();
+//day
+now.getDate();
+//day week
+now.getDay();
+//year
+now.getFullYear();
+//month
+now.getMonth();
+//update year
+now.setFullYear(2015);
