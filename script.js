206245b97302b6a15d01f2f9198c98d659ab35d5
mahan-redboy177 <mahanperspolis1386@gmail.com>
2025-09-20 11:23:07 +0330

    Add element to array

--- script.js
+++ script.js
@@ -1,17 +1,10 @@
-const now = new Date();
-const data1 = new Date('may 11 2026 09:00');
-const date2 = new Date(2018,4,11,9,0);
-//date
-now.toDateString();
-//date and hour
-now.toISOString();
-//day
-now.getDate();
-//day week
-now.getDay();
-//year
-now.getFullYear();
-//month
-now.getMonth();
-//update year
-now.setFullYear(2015);
+const numbers = [3,4];
+console.log(numbers)
+//انتها اضافه میکنه
+// numbers.push(5,6);
+//ابتدا اضافه میکنه
+// numbers.unshift(7,8);
+//مقدار اول دادن انذیس دوم اندیسی که حذف شود سوم المنت که اضافه شود در اندیس مشخص شده
+numbers.splice(2,0,"a")
+
+console.log(numbers);
