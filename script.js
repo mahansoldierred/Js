7bf1a3e0137a133516e2f118ac9c1e7e48de1a51
mahan-redboy177 <mahanperspolis1386@gmail.com>
2025-09-22 15:35:29 +0330

    Arrow Function

--- script.js
+++ script.js
@@ -1,34 +1,14 @@
-//primitive
-// const numbers = [1,2,3,1,5,1,4];
-// console.log(numbers.indexOf(1,1));
-// if(numbers.includes(1)){
-//     console.log("yes");
-// }
-//reference
-// const numbers = [
-//     {id : 1 , name : 'a'},
-//     {id : 2 , name : 'b'}
-// ];
-// console.log(numbers.includes({id : 1 , name:'a'}));
-
-// const courses = [
-//     {id : 1 , name:'a'},
-//     {id : 2 , name :'b'}
-// ];
-
-// const result = numbers.find(function(course){
-//     return course.name === "a"
-// })
-
-// console.log(result);
-
 const courses = [
     {id : 1 , name:'a'},
     {id : 2 , name:'b'}
 ];
+//فانکشن بی نام
+// const result = courses.find((course)=>{
+//     return course.name === 'a'
+// })
 
-const result = courses.findIndex(function(course){
-    return course.name === 'b'
-})
+// console.log(result);
+
+const result = courses.find(courses=> courses.name === 'a')
 
 console.log(result);
