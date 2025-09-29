a9eccfb19c065c994c5eee9af287c8dc629bcb98
mahan-redboy177 <mahanperspolis1386@gmail.com>
2025-09-29 20:33:31 +0330

    Array sorting

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const message = 'mahan coding is cool';
-const parts = message.split(' ');
-console.log(parts);
-const combined = parts.join('-');
-console.log(combined);
+const numbers = [
+    {id : 1 , name : 'html'},
+    {id : 2 , name : 'css'}
+];
+
+numbers.sort(function(a,b){
+    let nameA = a.name.toLowerCase();
+    let nameB = b.name.toLowerCase();
+    if(nameA<nameB) return -1;
+    if(nameB>nameA) return 1;
+    return 0 ;
+});
+
+console.log(numbers);
